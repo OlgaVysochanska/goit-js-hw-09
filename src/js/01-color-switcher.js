39fb9768fcbaccb3c8d9369180af682693f77612
OlgaVysochanska <olavysochanska@gmail.com>
2022-11-22 18:16:14 +0200

    task 1, switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startSwitchBtn = document.querySelector("[data-start]");
+const stopSwitchBtn = document.querySelector("[data-stop]");
+let start;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartSwitch() {
+    start = setInterval(() => {
+document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+}
+
+function onStopSwitch() {
+    clearInterval(start);
+}
+
+startSwitchBtn.addEventListener("click", onStartSwitch);
+stopSwitchBtn.addEventListener("click", onStopSwitch);
